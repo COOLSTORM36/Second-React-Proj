8cbb308c20a6c188b11cb428b1dec23393037c43
COOLSTORM36 <yoyo.sunshine@hotmail.com>
2025-01-30 23:14:15 +0000

    #

---
+++ src/components/header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Google Logo" />
+        <h1>Google</h1>
+    </header>
+  );
+}
